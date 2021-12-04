b2a77bb7cb7f54b39e833c095a69ec3bfec2741f
Srinitha Chinthala <sc21241@corp.ojas-it.com>
2021-12-04 12:17:15 +0530

    p1 and p2 file

--- p1.py
+++ p1.py
@@ -1,5 +1,5 @@
-print("hello hyderabad")
-if 10==10:
-  print("true")
+user_inp=int(input("enter your input"))
+if user_inp%2==0:
+    print("it is an even number")
 else:
-	print("else")
+    print("it is an odd number")
---
+++ p2.py
@@ -0,0 +1,5 @@
+user_inp=int(input("enter your input"))
+if user_inp%3==0:
+    print("it is divisable by 3")
+else:
+    print("it is not divisable by 3")
